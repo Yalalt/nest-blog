90c4d740fb98804b946382fc251fcba67cde4ae8
Yalalt <yalaltgandush@gmail.com>
2023-08-22 22:04:45 +0800

    edit some line on auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,8 +3,7 @@ import DiscordProvider from 'next-auth/providers/discord';
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import client from '@/lib/prisma';
 
-
-const handler = NextAuth({
+export const nextOptions = {
   providers: [
     DiscordProvider({
       clientId: process.env.DISCORD_CLIENT_ID || '',
@@ -15,6 +14,8 @@ const handler = NextAuth({
   pages: {
     signIn: '/signin',
   },
-} as NextAuthOptions);
+} as NextAuthOptions;
+
+const handler = NextAuth(nextOptions);
 
 export { handler as GET, handler as POST };
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,9 +1,19 @@
 import { FunctionComponent } from 'react';
 import Author from '@/components/Author';
+import { nextOptions } from '@/app/api/auth/[...nextauth]/route';
+import { getServerSession } from 'next-auth';
 
 interface PageProps {}
 
 const Page: FunctionComponent<PageProps> = async () => {
+
+  const session = await getServerSession(nextOptions);
+
+  if (!session) {
+    return <div>Not Authorized</div>
+  }
+
+  console.log(session, ' Session');
   const signInUserId = 1;
 
   return <Author authorId={signInUserId} />;
