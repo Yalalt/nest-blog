02d58f74cbeff0b578da1548a2fb1a9d8f95be77
Yalalt Gandush <yalaltgandush@gmail.com>
2024-01-24 23:55:05 +0800

    add document

--- src/app/api/user/post/route.ts
+++ src/app/api/user/post/route.ts
@@ -4,6 +4,13 @@ import { getServerSession } from 'next-auth';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function POST(request: NextRequest) {
+  /**
+   * Gets the user's session using NextAuth's getServerSession method.
+   *
+   * nextOptions contains the NextAuth options needed for getServerSession.
+   *
+   * Returns the user's session object containing user details like id, name etc.
+   */
   const session = await getServerSession(nextOptions);
 
   const post = await request.json();
