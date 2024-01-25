819b41952991e007718d2e8ca624c513bcb70561
Yalalt Gandush <yalaltgandush@gmail.com>
2024-01-26 00:30:18 +0800

    remove SSR

--- src/actions/posts.ts
+++ src/actions/posts.ts
@@ -1,16 +1,16 @@
-'use server';
+// 'use server';
+// this is server actions SSR
+// import { createPost, updatePost, deletePost } from '@/lib/prisma/posts';
+// import { Prisma } from '@prisma/client';
 
-import { createPost, updatePost, deletePost } from '@/lib/prisma/posts';
-import { Prisma } from '@prisma/client';
+// export const addPost = async (post: Prisma.PostCreateInput) => {
+//   return createPost(post);
+// };
 
-export const addPost = async (post: Prisma.PostCreateInput) => {
-  return createPost(post);
-};
+// export const editPost = async (id: string, post: Prisma.PostUpdateInput) => {
+//   return updatePost(id, post);
+// };
 
-export const editPost = async (id: string, post: Prisma.PostUpdateInput) => {
-  return updatePost(id, post);
-};
-
-export const removePost = async (id: string) => {
-  return deletePost(id);
-};
+// export const removePost = async (id: string) => {
+//   return deletePost(id);
+// };
