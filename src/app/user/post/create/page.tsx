af5948d82f0db9bd6077c03b15db6d2f0d74ea4f
Yalalt Gandush <yalaltgandush@gmail.com>
2024-01-26 00:31:02 +0800

    fix simple code

--- src/app/user/post/create/page.tsx
+++ src/app/user/post/create/page.tsx
@@ -1,10 +1,10 @@
-import Form from '@/components/post/Form';
+import BlogForm from '@/components/post/Form';
 import { FunctionComponent } from 'react';
 
 interface CreateProps {}
 
 const Create: FunctionComponent<CreateProps> = () => {
-  return <Form />;
+  return <BlogForm />;
 };
 
 export default Create;
